829a7a084d33c63a8c2bcbfb04ebbe9f07018164
Matheus Costa <matheus.costa@prestadores.finnet.com.br>
2024-08-25 05:15:14 -0300

    chore: reorganizing dev deps

--- server.ts
+++ server.ts
@@ -15,7 +15,6 @@ export type Response = {
 };
 
 router.post("/funds", (request, response) => {
-  console.log(request.headers);
   const uniqueid = String(request.headers.uniqueid);
 
   if (uniqueid === "undefined") {
